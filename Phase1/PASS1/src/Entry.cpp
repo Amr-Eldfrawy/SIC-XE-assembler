95c24c0b3a221691211f75305e0023289ec04df1
amreldfarwy <amr.f.eldfrawy@gmail.com>
2015-02-03 01:14:08 +0200

    Update Entry.cpp

--- Phase1/PASS1/src/Entry.cpp
+++ Phase1/PASS1/src/Entry.cpp
@@ -4,14 +4,14 @@
 Entry::Entry()
 {
     //ctor
-        loc=0;
-        label=op_code=operand=comment=error="";
+    loc=0;
+    label=op_code=operand=comment=error="";
 }
 
 Entry::Entry(int x , string a , string b , string c , string d,string e)
 {
-        loc = x;
-        label=a, op_code=b, operand=c ,comment=d,error=e;
+    loc = x;
+    label=a, op_code=b, operand=c ,comment=d,error=e;
 }
 
 Entry::~Entry()
